a9c578254308585fc4c5efbb66e5b2ee64c1b178
webwerks1 <aniketdalvi38@gmail.com>
2018-10-05 14:52:22 +0530

    api changes

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
-import axios from 'axios';
-import config from './config';
-import utils from './utils';
-import './storage';
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const admin = require("firebase-admin");
 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-//handling asynchronous call through axios
-var api = {
-    getData: function (baseURL) {
-        return new Promise(function (resolve, reject) {
-            axios.get(baseURL, config.apiconfig()).then(result => {
-                resolve(result.data)
-            }).catch(err => utils.alertMessage(err.message))
-        })
-    },
-    setData: function (name, lat, lang, type, phone, baseURL) {
-        return new Promise(function (resolve, reject) {
-            axios.post(baseURL, {
-                name: name,
-                lat: lat,
-                lang: lang,
-                type: type,
-                phone: phone
-            }, config.apiconfig()).then(result => {
-                console.log(result)
-                resolve(result.data)
-            }).catch(err => utils.alertMessage(err.message))
-        })
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://testmap-f1508.firebaseio.com"
+});
+
+// Get a database reference to our blog
+const db = admin.database();
+const ref = db.ref("/");
+const docRef = ref.child("places"); /* Targetting to collection */
+
+app.post("/set", function (req, res) {
+  const { name, lat, lang, type, phone } = req.body;
+  var obj = {
+    name: name,
+    lat: lat,
+    lang: lang,
+    type: type,
+    phone: phone
+  };
+
+  var data = {};
+  data[Date.now()] = obj;
+
+  docRef.update(data).then(function (err, response) {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(data)
     }
-}
+  });
+});
 
-export default api;
+app.get("/get", function (req, res) {
+  var newData = [];
+  /* get saved data from Firebase Database */
+  docRef.once("value", function (snapshot) {
+    /* Response From Firebase Database */
+    snapshot.forEach(function (data) {
+      const mainData = data.val();
+      newData.push(mainData);
+    });
+    res.send(newData);
+  });
+});
+
+app.listen(5500, function (err, res) {
+  if (!err) console.log("Server is Running");
+});
